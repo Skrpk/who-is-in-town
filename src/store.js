3e4cbb01ca5f2e4ce93d0ea65078dc53a687206d
vitaliy skripka <vitaliy.skripka23@gmail.com>
2019-01-03 22:27:33 +0200

    commit

---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import createSagaMiddleware from 'redux-saga';
+import createHistory from 'history/createBrowserHistory';
+
+import rootSaga from './sagas';
+import rootReducer from './rootReducer';
+
+export const history = createHistory();
+const sagaMiddleware = createSagaMiddleware();
+
+const initialState = {};
+const enhancers = [];
+const middleware = [
+  sagaMiddleware,
+  routerMiddleware(history)
+];
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
+
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  initialState,
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
